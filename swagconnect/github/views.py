6c0b34f5fc9f00c6eb5732239640b82515b7f698
1mace1 <belyaev.nikit@gmail.com>
2021-05-04 13:42:33 +0300

    Revert "1 all auth connectors"

--- swagconnect/github/views.py
+++
@@ -1,4 +0,0 @@
-from swagconnect.oauth2.views import OAuth2LoginView, OAuth2CallbackView, CustomOAuth2Adapter
-
-oauth_login = OAuth2LoginView.adapter_view(CustomOAuth2Adapter)
-oauth_callback = OAuth2CallbackView.adapter_view(CustomOAuth2Adapter)
