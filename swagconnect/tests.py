8f63620ac04b2926e4c69d90c6ff436b91324bfa
zarif99 <9685440@gmail.com>
2021-05-12 10:25:24 +0500

    test for github urls

--- swagconnect/tests.py
+++ swagconnect/tests.py
@@ -1,3 +1,14 @@
+from django.shortcuts import reverse
 from django.test import TestCase
 
-# Create your tests here.
+
+class Oauth2Test(TestCase):
+    def test_github_login(self):
+        url = reverse('github_login')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+
+    def test_github_callback_url(self):
+        url = reverse('github_callback')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
