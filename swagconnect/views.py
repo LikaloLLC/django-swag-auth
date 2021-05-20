20385cd5098499d3cf244ef08b8d650052975b1c
zarif99 <9685440@gmail.com>
2021-05-20 18:21:03 +0500

    endpoint swagger_storage

--- swagconnect/views.py
+++ swagconnect/views.py
@@ -16,9 +16,3 @@ class SwaggerStorageViewSet(ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-    # TODO:
-    #  1. Must accept only authenticated users (https://www.django-rest-framework.org/api-guide/authentication/)
-    #  2. Must pass current user to the model on save, which can be found in request.user (look at `.perform_create()`)
-    #  3. Add the remaining attributes to this viewset (https://www.django-rest-framework.org/api-guide/viewsets/)
-    #  This endpoint accepts ConnectorToken PK and url to the swagger. For testing, you should authenticate via github,
-    #  copy the PK of the ConnectorToken and pass it to this endpoint.
