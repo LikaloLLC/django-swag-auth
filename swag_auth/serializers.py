3bfbcf5463fe4153bd42086acdafa5d77e02c440
1mace1 <belyaev.nikit@gmail.com>
2021-06-10 22:26:35 +0300

    Removed creating a new SwaggerStorage record, when the same already exists in the db. It gets a new token and is updated.

--- swag_auth/serializers.py
+++ swag_auth/serializers.py
@@ -4,6 +4,12 @@ from .models import SwaggerStorage
 
 
 class SwaggerStorageSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+
     class Meta:
         model = SwaggerStorage
         fields = ('user', 'url', 'token')
+
--- swag_auth/views.py
+++ swag_auth/views.py
@@ -1,7 +1,11 @@
+from typing import Optional
+
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from swag_auth.models import SwaggerStorage
+from swag_auth.models import SwaggerStorage, ConnectorToken
 from swag_auth.serializers import SwaggerStorageSerializer
 
 
@@ -17,5 +21,26 @@ class SwaggerStorageViewSet(ModelViewSet):
         user = self.request.user
         return SwaggerStorage.objects.filter(user=user)
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+    def get_existing_storage(self, token_pk: str, url: str) -> Optional['SwaggerStorage']:
+        """Return SwaggerStorage, filtered by the specified user, url and token__connector, if exists."""
+        token = ConnectorToken.objects.get(pk=token_pk)
+
+        return SwaggerStorage.objects.filter(
+            user=self.request.user, url=url, token__connector=token.connector
+        ).last()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        # Check if the user already has a SwaggerStorage instance with the specified url and provider.
+        # If it exists, just update it with a new token,
+        # so we don't create multiple identical records.
+        existing_storage = self.get_existing_storage(request.data['token'], request.data['url'])
+        if existing_storage:
+            serializer = self.get_serializer(existing_storage, data={'token': serializer.data['token']})
+            serializer.is_valid(raise_exception=True)
+
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
