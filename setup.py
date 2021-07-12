77345e9f4b77ce59899cfc54da5c9958f5f1e3c4
1mace1 <belyaev.nikit@gmail.com>
2021-07-12 17:38:48 +0300

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="swag_auth",
-    version="0.0.1",
+    version="0.0.6",
     author="Likalo Limited",
     author_email="hello@docsie.io",
     description="This is an open source implementation of swag-auth",
