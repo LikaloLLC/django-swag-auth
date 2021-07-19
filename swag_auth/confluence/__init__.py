0351588635bcd99aab582ccbc22cdeb9bd706579
zarif99 <9685440@gmail.com>
2021-07-19 10:48:56 +0500

    Added ConfluenceConnector for getting token

---
+++ swag_auth/confluence/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'swag_auth.confluence.apps.ConfluenceAppConfig'
--- swag_auth/confluence/connectors.py
+++ swag_auth/confluence/connectors.py
@@ -2,6 +2,7 @@ from atlassian import Confluence
 from django.conf import settings
 
 from swag_auth.base import BaseAPIConnector, BaseSwaggerDownloader
+from swag_auth.oauth2.views import CustomOAuth2Adapter
 
 
 class ConfluenceAPIConnector(BaseAPIConnector):
@@ -14,7 +15,7 @@ class ConfluenceAPIConnector(BaseAPIConnector):
         oauth2_dict = {
             "client_id": settings.SWAGAUTH_SETTINGS['confluence']['APP']['client_id'],
             "token": token}
-        self.client = Confluence(url='', oauth2=oauth2_dict)
+        self.client = Confluence(url='https://api.atlassian.com', oauth2=oauth2_dict)
 
     def get_file_content(self, path: str):
         return
@@ -31,3 +32,18 @@ class ConfluenceSwaggerDownloader(BaseSwaggerDownloader):
 
     def get_extension(self, url: str) -> str:
         ...
+
+
+class ConfluenceConnector(CustomOAuth2Adapter):
+    provider_id = 'confluence'
+    access_token_url = "https://auth.atlassian.com/oauth/token"
+    authorize_url = "https://auth.atlassian.com/authorize"
+    client_id = settings.SWAGAUTH_SETTINGS[provider_id]['APP']['client_id']
+    secret = settings.SWAGAUTH_SETTINGS[provider_id]['APP']['secret']
+    scope = settings.SWAGAUTH_SETTINGS[provider_id]['SCOPE']
+    callback_url = 'http://127.0.0.1:8000/swag/confluence/login/callback/'
+
+    api_connector_class = ConfluenceSwaggerDownloader
+
+
+connector_classes = [ConfluenceConnector]
